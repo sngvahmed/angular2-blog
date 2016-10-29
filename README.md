abb3a0c7fb9897b448c5bb05d90d333d04d526ea
ahmed nasser <ahmed_nasser1993@gmail.com>
2016-10-29 23:33:44 +0200

    make custom directive

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+https://www.youtube.com/watch?v=_-CD_5YhJTA
+
 # angular2-blog
 the first application with angular 2 
--- app/courses.component.ts
+++ app/courses.component.ts
@@ -1,15 +1,18 @@
 import {Component} from 'angular2/core'
 import {CoursesService} from './courses.service'
+import {AutoGrowDirective} from './auto-grow.directive'
 
 @Component({
     selector : 'courses',
     template : `
                <h1>{{title}}</h1>
+               <input type="text" autoGrow />
                <ul>
                 <li *ngFor="#name of names"> {{name}}</li>
                </ul>
               `,
-    providers : [CoursesService]
+    providers : [CoursesService],
+    directives : [AutoGrowDirective]
 })
 export class CoursesComponent{
     title:string = "Fisrt angular 2 application ";
